ff07541ea72601086401fafabb9e4533aa811b35
Boboyog <38315956+Boboyog@users.noreply.github.com>
2018-04-13 14:59:20 +0800

    added the following model: Project Server Project and Project Server Project List  added the following action IProjectOnlineAPI GetProjectByGuid(string guid), GetProjectByName(string name) GetAllProjects()

--- ProjectOnlineMobile2/Services/ProjectOnlineApiWrapper.cs
+++ ProjectOnlineMobile2/Services/ProjectOnlineApiWrapper.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using ProjectOnlineMobile2.Models;
+using Refit;
 
 namespace ProjectOnlineMobile2.Services
 {
@@ -19,6 +23,45 @@ namespace ProjectOnlineMobile2.Services
                     BaseAddress = new Uri(_sharepointUrl)
                 };
                 _client.DefaultRequestHeaders.Accept.Add(mediaType);
+            }
+        }
+
+        public async Task<ProjectServerProject> GetProjectByGuid(string guid)
+        {
+            try
+            {
+                return await RestService.For<IProjectOnlineApi>(_client).GetProjectByGuid(guid);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public async Task<ProjectServerProject> GetProjectByName(string projectName)
+        {
+            try
+            {
+                return await RestService.For<IProjectOnlineApi>(_client).GetProjectByName(projectName);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public async Task<ProjectServerProjectList> GetAllProjects()
+        {
+            try
+            {
+                return await RestService.For<IProjectOnlineApi>(_client).GetAllProjects();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return null;
             }
         }
     }
