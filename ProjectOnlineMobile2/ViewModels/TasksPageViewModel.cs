cf82381573cd672ccd371628db27d4a8c24e7266
k-francisco <piattosnovalays@gmail.com>
2018-04-20 13:20:40 +0800

    finished filtering the task of the user. also made the views for the task page.

--- ProjectOnlineMobile2/ViewModels/TasksPageViewModel.cs
+++ ProjectOnlineMobile2/ViewModels/TasksPageViewModel.cs
@@ -1,4 +1,4 @@
-﻿using Result = ProjectOnlineMobile2.Models.TaskModel.Result;
+﻿using Result = ProjectOnlineMobile2.Models.ResourceAssignmentModel.Result;
 using ProjectOnlineMobile2.Models.ResourceAssignmentModel;
 using ProjectOnlineMobile2.Models.TaskModel;
 using System;
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Text;
 using Xamarin.Forms;
+using System.Linq;
 
 namespace ProjectOnlineMobile2.ViewModels
 {
@@ -22,51 +23,25 @@ namespace ProjectOnlineMobile2.ViewModels
         public TasksPageViewModel()
         {
             Tasks = new ObservableCollection<Result>();
-            if(Tasks.Count == 0)
+            MessagingCenter.Instance.Subscribe<String>(this, "GetTasks", (s) =>
             {
-                MessagingCenter.Instance.Subscribe<String>(this, "GetTasks", (s) =>
-                {
+                if(Tasks.Count == 0)
                     GetTasks();
-                });
-            }
+            });
         }
 
         private async void GetTasks()
         {
-            List<TaskModel> projectTasks = new List<TaskModel>();
-            List<string> taskIds = new List<string>();
             try
             {
                 foreach (var project in projects.D.Results)
                 {
-                    var tempProjectTasks = await PSapi.GetTasksByProject(project.ProjectId);
-                    projectTasks.Add(tempProjectTasks);
                     var resourceAssignments = await PSapi.GetResourceAssignment(project.ProjectId, userName);
                     foreach (var item in resourceAssignments.D.Results)
                     {
-                        taskIds.Add(item.TaskId);
+                        Tasks.Add(item);
                     }
                 }
-
-                foreach (var item in taskIds) {
-                    Debug.WriteLine("taskIds", item);
-                }
-
-                foreach (var item in projectTasks)
-                {
-                    foreach (var item2 in item.D.Results)
-                    {
-                        Debug.WriteLine("projectTasks", item2.Name);
-                    }
-                }
-
-                //foreach (var task in Tasks)
-                //{
-                //    foreach (var projectTask in task.D.Results)
-                //    {
-                //        Debug.WriteLine("GetTasks", projectTask.TaskName);
-                //    }
-                //}
             }
             catch(Exception e)
             {
